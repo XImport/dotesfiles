f3d7c609ab0e365bdf39a26a6e0ecb6eca4dc0fd
XImport <zed.robot99@gmail.com>
2021-04-16 02:51:59 +0000

    dmenu

---
+++ suckless/dmenu_suckless/config.def.h
@@ -0,0 +1,24 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Comic Mono:size=10", "JoyPixels:pixelsize=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e5e9f0", "#3b4252" },
+	[SchemeSel] = { "#ffffff", "#5e81ac" },
+	[SchemeOut] = { "#151E31", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
